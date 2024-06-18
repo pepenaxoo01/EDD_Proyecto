8a6bb80c6032947c800abbda023aa867b946af63
JKdagan <javier.cadagan6@gmail.com>
2024-06-17 21:48:27 -0400

    Huffman comentado y medicion de bits implementada

--- Huffman.h
+++ Huffman.h
@@ -1,6 +1,6 @@
 #pragma once
 
-//Referencia: https://www.geeksforgeeks.org/huffman-coding-greedy-algo-3/
+// Referencia: https://www.geeksforgeeks.org/huffman-coding-greedy-algo-3/
 
 #include "Nodo.h"
 #include <iostream>
@@ -10,110 +10,109 @@
 
 class Huffman {
 private:
-	std::string str_original;
-	std::string str_codificada;
-	std::map<char, std::string> tabla;
-	Nodo* raiz;
+    std::string str_original;   // String original a codificar
+    std::string str_codificada; // String codificado usando Huffman
+    std::map<char, std::string> tabla; // Tabla de codigos Huffman
+    Nodo* raiz; // Raiz del arbol de Huffman
 
-	void _obtenerCodigos(Nodo* raiz, std::string str) {
-		if (!raiz) {
-			return;
-		}
+    // Funcion recursiva para obtener los codigos Huffman
+    void _obtenerCodigos(Nodo* raiz, std::string str) {
+        if (!raiz) {
+            return;
+        }
 
-		if (raiz->dato != '$') {
-			tabla[raiz->dato] = str;
-		}
+        if (raiz->dato != '$') {
+            tabla[raiz->dato] = str;
+        }
 
-		_obtenerCodigos(raiz->izq, str + "0");
-		_obtenerCodigos(raiz->der, str + "1");
-	}
+        _obtenerCodigos(raiz->izq, str + "0");
+        _obtenerCodigos(raiz->der, str + "1");
+    }
 
-	std::string _codificarString(Nodo* raiz, const std::string& str) {
-
-		std::string str_codificada;
-		for (char c : str) {
-			str_codificada += tabla[c];
-		}
-
-		return str_codificada;
-	}
+    // Funcion para codificar un string usando la tabla de codigos Huffman
+    std::string _codificarString(Nodo* raiz, const std::string& str) {
+        std::string str_codificada;
+        for (char c : str) {
+            str_codificada += tabla[c];
+        }
+        return str_codificada;
+    }
 
 public:
+    // Funcion principal para codificar un string utilizando Huffman
+    std::string codificar(const std::string& str) {
+        // Cola de prioridad de nodos que representan los caracteres del string
+        std::priority_queue<Nodo*, std::vector<Nodo*>, compararNodos> cola;
 
-	//Funcion que recibe un string y devuelve un puntero a un nodo que representa el arbol de huffman
-	std::string codificar(const std::string& str) {
-		//Vector de nodos que representan los caracteres del string
-		std::priority_queue<Nodo*, std::vector<Nodo*>, compararNodos> cola;
+        // Conteo de frecuencias de cada caracter en el string
+        std::map<char, int> freq;
+        for (char c : str) {
+            freq[c]++;
+        }
 
-		//Se cuentan los caracteres del string
-		std::map<char, int> freq;
-		for (char c : str) {
-			freq[c]++;
-		}
+        // Creacion de nodos y colocacion en la cola de prioridad
+        for (auto i = freq.begin(); i != freq.end(); i++) {
+            cola.push(new Nodo(i->first, i->second));
+        }
 
-		//Se crean los nodos y se meten en la cola
-		for (auto i = freq.begin(); i != freq.end(); i++) {
-			cola.push(new Nodo(i->first, i->second));
-		}
+        Nodo* izq, * der, * top;
 
-		Nodo* izq, * der, * top;
+        // Construccion del arbol de Huffman combinando nodos de menor frecuencia
+        while (cola.size() != 1) {
+            // Sacamos los dos nodos con menor frecuencia
+            izq = cola.top();
+            cola.pop();
 
-		while (cola.size() != 1) {
-			//Sacamos los dos nodos con menor frecuencia
-			izq = cola.top();
-			cola.pop();
+            der = cola.top();
+            cola.pop();
 
-			der = cola.top();
-			cola.pop();
+            // Creamos un nuevo nodo interno con la suma de las frecuencias de los dos nodos sacados
+            // Los nodos sacados se convierten en hijos de este nuevo nodo
+            // El caracter '$' representa un nodo interno
+            top = new Nodo('$', izq->freq + der->freq);
+            top->izq = izq;
+            top->der = der;
 
-			//Creamos un nodo con la suma de las frecuencias de los dos nodos anteriores
-			//Los nodos sacados se convierten en hijos de este nodo
-			//Asumiremos que el caracter '$' no se usara y es para representar un nodo interno
-			top = new Nodo('$', izq->freq + der->freq);
-			top->izq = izq;
-			top->der = der;
+            cola.push(top);
+        }
 
-			cola.push(top);
-		}
+        raiz = cola.top(); // La raiz del arbol es el unico nodo restante en la cola
+        _obtenerCodigos(raiz, ""); // Obtenemos los codigos Huffman
+        str_codificada = _codificarString(raiz, str); // Codificamos el string original
+        return str_codificada;
+    }
 
-		raiz = cola.top();
-		_obtenerCodigos(raiz, "");
-		str_codificada = _codificarString(raiz, str);
-		return str_codificada;
-	}
+    // Funcion para decodificar un string codificado usando el arbol de Huffman
+    std::string decodificar(const std::string& str) {
+        std::string str_decodificada;
+        Nodo* temp = raiz;
 
-	std::string decodificar(const std::string& str) {
-		std::string str_decodificada;
-		Nodo* temp = raiz;
+        // Recorremos el string codificado caracter por caracter
+        for (char c : str) {
+            if (c == '0') {
+                temp = temp->izq; // Movemos hacia el hijo izquierdo si encontramos '0'
+            }
+            else {
+                temp = temp->der; // Movemos hacia el hijo derecho si encontramos '1'
+            }
 
-		for (char c : str) {
-			if (c == '0') {
-				temp = temp->izq;
-			}
-			else {
-				temp = temp->der;
-			}
+            // Si llegamos a una hoja, agregamos el caracter correspondiente a la cadena decodificada
+            if (temp->isHoja()) {
+                str_decodificada += temp->dato;
+                temp = raiz; // Reiniciamos el recorrido desde la raiz
+            }
+        }
 
-			if (temp->isHoja()) {
-				str_decodificada += temp->dato;
-				temp = raiz;
-			}
-		}
+        return str_decodificada;
+    }
 
-		return str_decodificada;
-	}
+    // Funcion para obtener el string codificado
+    std::string getStringCodificada() {
+        return str_codificada;
+    }
 
-	std::string getStringCodificada() {
-		return str_codificada;
-	}
-
-	std::map <char, std::string> getTabla() {
-		return tabla;
-	}
+    // Funcion para obtener la tabla de codigos Huffman
+    std::map<char, std::string> getTabla() {
+        return tabla;
+    }
 };
-
-
-
-
-
-
--- main.cpp
+++ main.cpp
@@ -1,17 +1,16 @@
 #include "Huffman.h"
+#include "medir_compresion.h"
 #include <iostream>
 #include <string>
 
 int main(void) {
-	std::string str = "alabar_a_la_alabarda";
 	Huffman huffman;
-
+	std::string str = "alabar_a_la_alabarda";
 	std::string str_cod = huffman.codificar(str);
 	std::cout << "Codificado: " << str_cod << std::endl;
 	std::cout << "Decodificado: " << huffman.decodificar(str_cod) << std::endl;
 
-	str = "abalarducha_alabardin";
-	str_cod = huffman.codificar(str);
-	std::cout << "Codificado: " << str_cod << std::endl;
-	std::cout << "Decodificado: " << huffman.decodificar(str_cod) << std::endl;
+	print_size_bits(str);
+	print_size_bits(str_cod);
+
 }
